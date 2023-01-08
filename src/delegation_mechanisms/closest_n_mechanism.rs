7603f2a176e5492dd03538876affd69793076698
Michael Hegerhorst <michael.hegerhorst@gmail.com>
2023-01-07 20:54:22 -0700

    ClosestNMechanism

--- src/delegation_mechanisms/closest_n_mechanism.rs
+++ src/delegation_mechanisms/closest_n_mechanism.rs
@@ -1,5 +1,6 @@
 use crate::delegation_mechanisms::DelegationMechanism;
-use crate::{Rankings, TruthEstimator};
+use crate::utils::calculate_distance;
+use crate::{Ranking, Rankings, TruthEstimator};
 
 pub struct ClosestNMechanism {
     n: u32,
@@ -7,6 +8,9 @@ pub struct ClosestNMechanism {
 
 impl ClosestNMechanism {
     fn new(n: u32) -> ClosestNMechanism {
+        if n == 0 {
+            panic!("n must be greater than 0");
+        }
         ClosestNMechanism { n }
     }
 }
@@ -17,7 +21,28 @@ impl<'a> DelegationMechanism<'a> for ClosestNMechanism {
         agent: &dyn TruthEstimator,
         proxies: &[&'a dyn TruthEstimator],
     ) -> Rankings<'a> {
-        todo!("Implement ClosestNMechanism::delegate")
+        // Assign the closest proxy all the weight, order the rest by distance
+        let mut sorted_proxies = proxies
+            .iter()
+            .map(|&p| (p, calculate_distance(agent, p)))
+            .collect::<Vec<_>>();
+        sorted_proxies.sort_by(|(_, d1), (_, d2)| d1.partial_cmp(d2).unwrap());
+
+        let mut weights = vec![0.0; sorted_proxies.len()];
+        let weight = 1.0 / self.n as f64;
+        for i in 0..self.n as usize {
+            weights[i] = weight;
+        }
+
+        let ranking_vec = sorted_proxies
+            .into_iter()
+            .map(|(p, _)| p)
+            .enumerate()
+            .zip(weights)
+            .map(|((rank, p), w)| Ranking::new(p, rank as u32, w))
+            .collect();
+
+        Rankings::new(ranking_vec)
     }
 }
 
