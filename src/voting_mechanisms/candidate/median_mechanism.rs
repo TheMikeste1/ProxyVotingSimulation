7e54946fb8c309945be58c5252f980a476c55021
Michael Hegerhorst <michael.hegerhorst@gmail.com>
2023-01-14 19:28:02 -0700

    Median mechanism

--- src/voting_mechanisms/candidate/median_mechanism.rs
+++ src/voting_mechanisms/candidate/median_mechanism.rs
@@ -1,6 +1,9 @@
 use crate::prelude::{Rankings, TruthEstimator};
+use crate::utils::{sum_rankings_weights, ProxyWeightSum};
 use crate::voting_mechanisms::VotingMechanism;
-use crate::Truth;
+use crate::{Truth, Weight};
+use ordered_float::OrderedFloat;
+use std::cmp::Ordering;
 use std::rc::Rc;
 
 pub struct MedianMechanism;
@@ -10,8 +13,25 @@ impl VotingMechanism for MedianMechanism {
         &mut self,
         _proxies: &[Rc<dyn TruthEstimator>],
         _delegators: &[Rc<dyn TruthEstimator>],
-        _rankings: &[Rankings],
+        rankings: &[Rankings],
     ) -> Truth {
-        todo!("Implement MedianMechanism::solve")
+        let proxy_weights = sum_rankings_weights(rankings);
+        let total_weight = proxy_weights.iter().map(|r| r.weight).sum::<Weight>();
+
+        let median_weight = total_weight / 2.0;
+        let mut current_weight: Weight = OrderedFloat(0.0);
+        // Find the median value
+        for (i, ProxyWeightSum { proxy, weight }) in proxy_weights.iter().enumerate() {
+            current_weight += weight;
+            if current_weight >= median_weight {
+                return proxy.get_last_estimate().unwrap();
+            }
+        }
+        unreachable!(
+            "The median weight should always be found. \
+        Is the total weight more than the sum of the weights? \
+        Median weight: {}, total weight: {}",
+            median_weight, total_weight
+        );
     }
 }
